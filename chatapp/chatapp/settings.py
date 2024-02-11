63b8ccd10659799b1bc2a80e0d1648ccc1a51391
Ali-404 <abdellali.aitaddi@gmail.com>
2024-02-11 21:38:28 +0100

    make it better

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-%hoz=*uo)n2qgw5s=5t)cx$f1x$dvgy*#q6aldm8n)7)4@nk%7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+
 
 
 # Application definition
